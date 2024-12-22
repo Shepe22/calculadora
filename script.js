2701a40c96ac5582ada028ee430361cf485f8ac9
Pedro González Rodríguez <Shepe22>
2024-12-22 14:51:12 +0100

    Casi pero no :(

--- script.js
+++ script.js
@@ -113,9 +113,11 @@ function cero() {
 // <<<<<<<<< OPERACIONES >>>>>>>>>
 
 function division() {
+    let simbolos = pantalla.split("");
+    let longitud = simbolos.length;
     if (pantalla == undefined) {
 
-    } else if (/[/*\-+.]{1,}/.test(pantalla)) {
+    } else if (simbolos[longitud - 1] == "/") {
         return parrafo.innerHTML = pantalla;
     } else {
         pantalla += "/";
@@ -124,9 +126,11 @@ function division() {
 }
 
 function multiplicacion() {
+    let simbolos = pantalla.split("");
+    let longitud = simbolos.length;
     if (pantalla == undefined) {
 
-    } else if (/[/*\-+.]{1,}/.test(pantalla)) {
+    } else if (simbolos[longitud - 1] == "*") {
         return parrafo.innerHTML = pantalla;
     } else {
         pantalla += "*";
@@ -135,9 +139,11 @@ function multiplicacion() {
 }
 
 function resta() {
+    let simbolos = pantalla.split("");
+    let longitud = simbolos.length;
     if (pantalla == undefined) {
 
-    } else if (/[/*\-+.]{1,}/.test(pantalla)) {
+    } else if (simbolos[longitud - 1] == "-") {
         return parrafo.innerHTML = pantalla;
     } else {
         pantalla += "-";
@@ -146,9 +152,11 @@ function resta() {
 }
 
 function suma() {
+    let simbolos = pantalla.split("");
+    let longitud = simbolos.length;
     if (pantalla == undefined) {
 
-    } else if (/[/*\-+.]{1,}/.test(pantalla)) {
+    } else if (simbolos[longitud - 1] == "+") {
         return parrafo.innerHTML = pantalla;
     } else {
         pantalla += "+";
@@ -157,9 +165,11 @@ function suma() {
 }
 
 function punto() {
+    let simbolos = pantalla.split("");
+    let longitud = simbolos.length;
     if (pantalla == undefined) {
 
-    } else if (/[/*\-+.]{1,}/.test(pantalla)) {
+    } else if (simbolos[longitud - 1] == ".") {
         return parrafo.innerHTML = pantalla;
     } else {
         pantalla += ".";
@@ -168,31 +178,93 @@ function punto() {
 }
 
 
-// <<<<<<<<< OPERACIONES >>>>>>>>>
+// <<<<<<<<< RESULTADO >>>>>>>>>
 
-function resultado() {
-    let operacion = [];
-    if (pantalla.includes("+")) {
-        operacion = pantalla.split("+");
-        pantalla = Number(operacion[0]) + Number(operacion[1]);
-        return parrafo.innerHTML = pantalla;
+function resultadoDivision(arr) {
+    let division = arr.split(/(\/)/);
+    let posicion = division.indexOf("/");
+    if (posicion == - 1) {
+        return arr;
+    }
+    while (posicion !== -1) {
 
-    } else if (pantalla.includes("-")) {
-        operacion = pantalla.split("-");
-        pantalla = Number(operacion[0]) - Number(operacion[1]);
-        return parrafo.innerHTML = pantalla;
+        let num1 = Number(division[posicion - 1]) 
+        let num2 = Number(division[posicion + 1]);
+        let result = num1 / num2;
+        result = result % 1 == 0 ? result : result.toFixed(2)
+        division.splice(posicion - 1, 3, result.toString());
+        posicion = division.indexOf("/");
+        console.log(result);
+    }
+    return division.join("");
+}
 
-    } else if (pantalla.includes("*")) {
-        operacion = pantalla.split("*");
-        pantalla = Number(operacion[0]) * Number(operacion[1]);
-        return parrafo.innerHTML = pantalla;
-        
-    } else if (pantalla.includes("/")) {
-        operacion = pantalla.split("/");
-        pantalla = Number(operacion[0]) / Number(operacion[1]);
-        return parrafo.innerHTML = pantalla;
-        
+function resultadoMultiplicacion(arr) {
+    let multiplicacion = arr.split(/(\*)/);
+    let posicion = multiplicacion.indexOf("*");
+    if (posicion == - 1) {
+        return arr;
+    }else{
+
+    }
+    while (posicion != -1) {
+
+        let result = Number(multiplicacion[posicion - 1]) * Number(multiplicacion[posicion + 1]);
+        result = result % 1 == 0 ? result : result.toFixed(2)
+        multiplicacion.splice(posicion - 1, 3, result);
+        posicion = multiplicacion.indexOf("*");
     }
 
+    return multiplicacion.join("");
+}
 
+function resultadoSuma(arr) {
+    let suma = arr.split(/(\+)/);
+    let posicion = suma.indexOf("+");
+    if (posicion == - 1) {
+        return arr;
+    }
+    while (posicion != -1) {
+
+        let num1 = Number(suma[posicion - 1]) 
+        let num2 = Number(suma[posicion + 1]);
+        let result = num1 / num2;
+        result = result % 1 == 0 ? result : result.toFixed(2)
+        suma.splice(posicion - 1, 3, result);
+        posicion = suma.indexOf("+");
+    }
+
+    return suma.join("");
+}
+
+function resultadoResta(arr) {
+    let resta = arr.split(/(\-)/);
+    let posicion = resta.indexOf("-");
+    if (posicion == - 1) {
+        return arr;
+    }
+    while (posicion != -1) {
+
+        let result = Number(resta[posicion - 1]) - Number(resta[posicion + 1]);
+        result = result % 1 == 0 ? result : result.toFixed(2)
+        resta.splice(posicion - 1, 3, result);
+        posicion = resta.indexOf("-");
+    }
+
+    return resta.join("");
+}
+
+
+
+
+
+function resultado() {
+
+    let result = pantalla;
+    result = resultadoDivision(result);
+    result = resultadoMultiplicacion(result);
+    result = resultadoSuma(result);
+    result = resultadoResta(result);
+    pantalla = result;
+    return parrafo.innerHTML = result;
 }
